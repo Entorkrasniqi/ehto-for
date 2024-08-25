7595f64d626b46a163e7014b886fc69f67654db0
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2024-08-25 16:49:43 +0300

    update

--- t1/t1.js
+++ t1/t1.js
@@ -1,10 +1,20 @@
+
 function TemperatureConverter() {
+  const celsius = parseFloat(document.getElementById("celsius").value);
 
-  const celsius = document.getElementById(celsius).value;
-
-
+  // Converting celsius to fahrenheit tingi.
   const fahrenheit = (celsius * 9/5) + 32;
 
+  const KelvinScale = -273.15;
+  const kelvin = celsius - KelvinScale;
 
-  const kelvin = parseFloat
+  document.getElementById("fahrenheit").innerText = `Lämpötila Fahrenheit-asteina: ${fahrenheit.toFixed(2)} °F`;
+  document.getElementById("kelvin").innerText = `Lämpötila Kelvineinä: ${kelvin.toFixed(2)} K`;
 }
+
+// This is the way how to do it by pressing enter.
+document.getElementById("celsius").addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+      TemperatureConverter();
+  }
+});
